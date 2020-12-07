77b915692ee71bac4e1fa97905cc8b1630979201
Michael Beaumont <mjboamail@gmail.com>
2020-12-07 20:58:14 +0100

    recently-contributed: Use logo and logoColor

--- recently-contributed/src/template.test.ts
+++ recently-contributed/src/template.test.ts
@@ -8,3 +8,32 @@ describe("joinWithFinalSep", () => {
   });
 });
 
+describe("makeShield", () => {
+  it("no link, no logo", () => {
+    expect(template.makeShield("x", "#ffffff", "", false)).toStrictEqual(
+      "![x](https://img.shields.io/static/v1?label=&message=x&color=%23ffffff)"
+    );
+  });
+  it("no link, logo", () => {
+    expect(template.makeShield("x", "#000000", "", true)).toStrictEqual(
+      "![x](https://img.shields.io/static/v1?logo=x&logoColor=%23fff&label=&message=x&color=%23000000)"
+    );
+  });
+  it("with light background", () => {
+    expect(template.makeShield("x", "#dea584", "", true)).toStrictEqual(
+      "![x](https://img.shields.io/static/v1?logo=x&logoColor=%23333&label=&message=x&color=%23dea584)"
+    );
+  });
+  it("link, no logo", () => {
+    expect(
+      template.makeShield("x", "#ffffff", "linkhref", false)
+    ).toStrictEqual(
+      '<a href="linkhref"><img src="https://img.shields.io/static/v1?label=&message=x&color=%23ffffff" alt=x/></a>'
+    );
+  });
+  it("link, logo", () => {
+    expect(template.makeShield("x", "#ffffff", "linkhref", true)).toStrictEqual(
+      '<a href="linkhref"><img src="https://img.shields.io/static/v1?logo=x&logoColor=%23333&label=&message=x&color=%23ffffff" alt=x/></a>'
+    );
+  });
+});
--- recently-contributed/src/template.ts
+++ recently-contributed/src/template.ts
@@ -1,15 +1,81 @@
-function makeShield(message: string, color: string, link: string): string {
+const cssColorConverter = require("css-color-converter");
+
+const brightnessThreshold = 0.69;
+
+const logos: { [k: string]: string } = {
+  Shell: "gnu bash",
+  shell: "gnu bash",
+};
+
+function getLogoName(text: string): string {
+  return encodeURIComponent(logos[text] ?? text);
+}
+
+function brightness(color: string) {
+  if (color) {
+    const rgb = cssColorConverter.fromString(color).toRgbaArray();
+    if (rgb) {
+      return +((rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 255000).toFixed(
+        2
+      );
+    }
+  }
+  return 0;
+}
+
+function colorsForBackground(
+  color: string
+): { textColor: string; shadowColor: string } {
+  if (brightness(color) <= brightnessThreshold) {
+    return {
+      textColor: "#fff",
+      shadowColor: "#010101",
+    };
+  }
+  return {
+    textColor: "#333",
+    shadowColor: "#ccc",
+  };
+}
+
+export function makeShield(
+  message: string,
+  color: string,
+  link: string,
+  useLogo: boolean = false
+): string {
+  const params = [
+    useLogo
+      ? [
+          ["logo", getLogoName(message)],
+          [
+            "logoColor",
+            encodeURIComponent(colorsForBackground(color).textColor),
+          ],
+        ]
+      : [],
+    [
+      ["label", ""],
+      ["message", message],
+      ["color", encodeURIComponent(color)],
+    ],
+  ]
+    .flat()
+    .map((comps) => comps.join("="))
+    .join("&");
   if (link) {
-    const img = `<img src="https://img.shields.io/static/v1?label=&message=${message}&color=${color}" alt=${message}/>`;
+    const img = `<img src="https://img.shields.io/static/v1?${params}" alt=${message}/>`;
     return `<a href="${link}">${img}</a>`;
   } else {
-    return `![${message}](https://img.shields.io/static/v1?label=&message=${message}&color=${encodeURIComponent(
-      color
-    )})`;
+    return `![${message}](https://img.shields.io/static/v1?${params})`;
   }
 }
 
-export function joinWithFinalSep(ss: string[], sep: string, finalSep: string): string {
+export function joinWithFinalSep(
+  ss: string[],
+  sep: string,
+  finalSep: string
+): string {
   const last = ss[ss.length - 1];
   const init = ss.slice(0, -1);
   return `${init.join(sep)}${init.length ? finalSep : ""}${last}`;
